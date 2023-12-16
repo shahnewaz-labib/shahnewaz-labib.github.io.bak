19bbd89f6cee6d954003c305fef62ebc7127c9aa
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2023-12-16 23:10:55 +0600

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astronaut.github.io',
+  // base: '/',
   integrations: [tailwind(), alpinejs()],
   output: 'server',
   adapter: vercel({
